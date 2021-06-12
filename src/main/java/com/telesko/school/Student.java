13ec2b93487fdd99e113339879672b40b01878eb
Muhammad Ahsan <mahsansoftengineer@indusvalley.io>
2021-06-12 10:43:02 +0500

    Required Changes for 2nd Level Cache

--- src/main/java/com/telesko/school/Student.java
+++ src/main/java/com/telesko/school/Student.java
@@ -1,23 +1,27 @@
-package com.telesko.school;
+    package com.telesko.school;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.*;
+    import javax.persistence.*;
 
-import javax.persistence.*;
-@Entity
-public class Student {
-  @Id
-  private int id;
-  private String name;
-  @OneToMany(mappedBy = "student", fetch = FetchType.EAGER)
-  public List<Laptop> laptop = new ArrayList<Laptop>();
-  public Student(){}
-  public Student(int id, String name){
-    this.id = id;
-    this.name = name;
-  }
-  @Override
-  public String toString(){
-    return " Student : { " + id + " : " + name + " : " + laptop.toString()  + " }"; 
-  } 
-}
+    // import org.hibernate.annotations.Cache;
+    // import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+    @Entity
+    // @Cacheable
+    // @Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+    public class Student {
+      @Id
+      private int id;
+      private String name;
+      @OneToMany(mappedBy = "student", fetch = FetchType.EAGER)
+      public List<Laptop> laptop = new ArrayList<Laptop>();
+      public Student(){}
+      public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+      }
+      @Override
+      public String toString(){
+        return " Student : { " + id + " : " + name + " : " + laptop.toString()  + " }"; 
+      } 
+    }
